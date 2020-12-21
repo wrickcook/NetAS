f561229954c11f0e64feb87fce3ccc00cf9ac76f
wrickcook <rickcook22@gmail.com>
2020-12-21 15:47:16 -0800

    initial check in

---
+++ NetAS-CMS/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Umbraco.Web.Models.ContentModel
+{
+    public class LoginModel
+    {
+        [Display(Name = "UserName")]
+        [Required]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
